928824dd247e8698c916962afc8d39d4f689e89c
Suraj Meena <surajmeena.dev@gmail.com>
2024-05-05 21:26:56 +0530

    Update location_input.dart

--- favorite_places_app/lib/widgets/location_input.dart
+++ favorite_places_app/lib/widgets/location_input.dart
@@ -28,7 +28,7 @@ class _LocationInputState extends State<LocationInput> {
     }
     final lat = _pickedLocation!.latitude;
     final lng = _pickedLocation!.longitude;
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyDLcwxUggpPZo8lcbH0TB4Crq5SJjtj4agSuraj';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyDLcwxUggpPZo8lcbH0TB4Crq5SJjtj4agSURAJMEENA';
   }
 
   Future<void> _savePlace(double latitude, double longitude) async {
